4b29cfeb2e40f073acc90903f6f5f93bd7faa1ab
Raven <ravenkoehler@gmail.com>
2021-07-03 17:36:45 -0400

    commit tree f191ed8af97ed94f3d1b91a3e781f2b04dd23496

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank?}
 
   validates_presence_of :name, :content
+
+  # def tags_attributes=(tags_attributes)
+  #   @tag = Tag.create(name: tags_attributes['name'])
+  #   self.tags << @tag
+  # end
+
 end
